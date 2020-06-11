b87e2fe30f2c5f903c27d87ec67c99f5c53b438a
Phony Lou <louqilin555@gmail.com>
2020-06-11 16:30:10 +0800

    Create upload-s3.sh

---
+++ qilin-metrics-feeder/upload-s3.sh
@@ -0,0 +1,4 @@
+#aws s3api create-bucket --bucket qilin-metrics-feeder --region ap-southeast-1 \
+#  --create-bucket-configuration LocationConstraint=ap-southeast-1
+
+aws s3 cp $PWD/target/qilin-metrics-feeder.jar s3://qilin-metrics-feeder
